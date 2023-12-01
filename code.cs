90272355d5311f5138e75d8ad4b826248f31d2e2
murillo dev72 <148728763+murillodev72@users.noreply.github.com>
2023-12-01 15:38:23 -0300

    Add files via upload

---
+++ code.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using CSharpShellCore;
+namespace JogoDeAdvinharNúmero{
+class Classe{
+static void Main(String[] args){
+Random random = new Random();
+bool jogarDeNovo = true;
+int min = 1;
+int max = 57;
+int palpite;
+int num;
+int tentativas;
+String resposta;
+
+
+while(jogarDeNovo){
+	palpite = 0;
+	resposta = "";
+	tentativas = 0;
+	num = random.Next(min, max +1);
+	
+	while(palpite != num){
+	Console.WriteLine("insira um número entre " + min + " - " + max);
+	palpite = Convert.ToInt32(Console.ReadLine());
+	Console.WriteLine("seu palpite: "+ palpite);
+	
+	if(palpite > num){
+	Console.WriteLine(palpite+ " é muito alto ");
+	
+	}
+	else if(palpite < num){
+	Console.WriteLine(palpite+" é muito baixo ");
+	
+	}
+	
+	tentativas++;
+	
+	}
+	Console.WriteLine("número: "+ num);
+	Console.WriteLine("VOCÊ VENCEU! ");
+	Console.WriteLine("tentativas: "+ tentativas);
+	
+	Console.WriteLine("quer jogar novamente? (S/N:)  ");
+	resposta = Console.ReadLine();
+	resposta = resposta.ToUpper();
+	if(resposta == "S"){
+	jogarDeNovo = true;
+	
+	}
+	else{
+		jogarDeNovo = false;
+	}
+}
+Console.WriteLine("obrigado por jogar");
+}
+}
+}
